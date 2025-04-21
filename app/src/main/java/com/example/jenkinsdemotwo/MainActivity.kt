08e01c5bf6943801dfb4571364a00e213a2240b0
eddiezh-520 <xiezhen520po@gmail.com>
2025-04-21 23:50:28 +0800

    update

--- app/src/main/java/com/example/jenkinsdemotwo/MainActivity.kt
+++ app/src/main/java/com/example/jenkinsdemotwo/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            //new comments
+            //new
             JenkinsDemoTwoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
